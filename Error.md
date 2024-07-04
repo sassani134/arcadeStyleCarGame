24f899669defee243aae69b45c9ea631521cd712
Samuel <sassani134@gmail.com>
2024-07-04 18:59:04 +0200

    Add a lot of assets scenes scripts md file texte to recreate the tuto environment

---
+++ Error.md
@@ -0,0 +1,16 @@
+Godot Engine v4.2.stable.official (c) 2007-present Juan Linietsky, Ariel Manzur & Godot Contributors.
+  modules/gltf/register_types.cpp:63 - L'import des fichiers blend est activé dans les paramètres du projet, mais aucun chemin vers Blender n'est configuré. Les fichiers blend ne seront pas importés.
+--- Debug adapter server started ---
+--- GDScript language server started on port 6005 ---
+Ajouter le Chargement Automatique
+Définir gravity_scale
+Définir physics_material_override
+Définir bounce
+Créer un nœud
+Rendre visible
+Attacher un script
+Supprimer le(s) nœud(s)
+Instantiate Scene(s)
+Translate
+Coller le(s) nœud(s) comme enfant(s) de car2
+  (4740) scene/3d/node_3d.cpp:343 - Condition "!is_inside_tree()" is true. Returning: Transform3D()
---
+++ Scripts/car2.gd
@@ -0,0 +1,41 @@
+extends RigidBody3D
+
+
+@onready var car_mesh = $CarMesh
+@onready var body_mesh = $CarMesh/suv2
+@onready var ground_ray = $CarMesh/RayCast3D
+@onready var right_wheel = $CarMesh/suv2/wheel_frontRight
+@onready var left_wheel = $CarMesh/suv2/wheel_frontLeft
+
+# Where to place the car mesh relative to the sphere
+var sphere_offset = Vector3.DOWN
+# Engine power
+var acceleration = 35.0
+# Turn amount, in degrees
+var steering = 18.0
+# How quickly the car turns
+var turn_speed = 4.0
+# Below this speed, the car doesn't turn
+var turn_stop_limit = 0.75
+
+# Variables for input values
+var speed_input = 0
+var turn_input = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	car_mesh.position = position + sphere_offset
+	if ground_ray.is_colliding():
+		apply_central_force(-car_mesh.global_transform.basis.z * speed_input)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if not ground_ray.is_colliding():
+		return
+	speed_input = Input.get_axis("brake", "accelerate") * acceleration
+	turn_input = Input.get_axis("steer_right", "steer_left") * deg_to_rad(steering)
+	right_wheel.rotation.y = turn_input
+	left_wheel.rotation.y = turn_input
